17143b89a435d082a2cf7c418c1c8eea8843aa91
sousakak7 <sousakak7@gmail.com>
2023-05-30 18:37:23 +0900

    static map

--- regionlist/i18n.lua
+++ regionlist/i18n.lua
@@ -1,5 +1,9 @@
 return {
     error {
         coordNotFound = "座標が欠けています&nbsp;:&nbsp;"
+        mapNotFound = "地図の画像が見つかりません"
+    },
+    settings {
+        mapAlign = "right"
     }
 }
--- regionlist/regionlist.lua
+++ regionlist/regionlist.lua
@@ -30,22 +30,51 @@ end
 
 function p._regionlist(args)
     entity = mw.wikibase.getEntityIdForCurrentPage()
+    staticMap = {
+        --[[
+            staticMap
+            - A pseudo class
+            - stores the static map data
+
+            args
+            - self.file mw.title: Required
+            - self.size string: (default: 350px)
+            - self.text string: (default: "{{BASEPAGENAME}}の地図")
+
+            vars
+            - self.name string: Name of the map. Not contain a namespace prefix.
+
+            funcs
+            - self.getWikitext: Get a wikitext which show the map on the page.
+        ]]
+    }
+    staticMap.new = function(file, size, text)
+        local obj = {}
+        obj.file = file
+        obj.size = size or "350px"
+        obj.text = text or (mw.title.getCurrentTitle().subpageText + "の地図")
+        obj.name = self.file.text
+        obj.getWikitext = function(self)
+            return "[[" + self.file.fullText + "|thumb|" + config.settings.mapAlign + "|" + self.size + "|" + self.text + "]]"
+        end
+    end
     if entity ~= nil then existWikidata = true else existWikidata = false end
     if yesno(args.mapframe) == true then
         maps = {}
         --[[
             maps = {
-                lat = (string, mayn't be there),
-                long = (string, mayn't be there),
-                zoom = (string, default: "auto"),
-                width = (string, default: ""),
-                height = (string, default: "")
+                lat       string: mayn't be there
+                long      string: mayn't be there
+                zoom      string: (default: "auto")
+                width     string: (default: "")
+                height    string: (default: "")
+                static staticMap: A pseudo class defined above. When there isn't a static map, this will be nil.
             }
         ]]
         if args.mapframelat ~= nil or args.mapframelong ~= nil then
             if args.mapframelat ~= nil then maps[lat] = coord(args.mapframelat, "", 6).dec else error(config.error.coordNotFound + "lat") end
             if args.mapframelong ~= nil then maps[long] = coord(args.mapframelong, "", 6).dec else error(config.error.coordNotFound + "long") end
-        else if existWikidata and entity:getBestStatements('P625')[1] ~= nil then
+        elseif existWikidata and entity:getBestStatements('P625')[1] ~= nil then
             maps[lat] = entity:getBestStatements('P625')[1].mainsnak.datavalue.value.latitude
             maps[long] = entity:getBestStatements('P625')[1].mainsnak.datavalue.value.longitude
         end
@@ -54,7 +83,11 @@ function p._regionlist(args)
         maps[height] = args.mapframeheight or ""
 
         if args.regionmap ~= nil then
-            --
+            mapfile = mw.title.new( args.regionmap, 6 )
+            if not(mapfile.file.exists()) then
+                error(config.error.mapNotFound)
+            else
+                maps[static] = staticMap.new(mapfile, , ) -- 作り途中
         end
     end
 end
