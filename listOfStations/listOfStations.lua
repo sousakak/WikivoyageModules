daa835643266f7b17aca5fdf269fba64053e0f11
sousakak7 <sousakak7@gmail.com>
2023-09-30 20:19:00 +0900

    update

--- listOfStations/listOfStations.lua
+++ listOfStations/listOfStations.lua
@@ -3,6 +3,16 @@ local p = {}
 local getArgs = require( 'Module:Arguments' ).getArgs
 local title = require( 'Module:BASICPAGENAME' ).BASICPAGENAME
 
+/* i18n */
+local i18n = {
+    css = '駅一覧/styles.css',
+    header_num = "駅番号",
+    header_name = "駅名",
+    header_tfr = "乗り換え路線",
+    header_spot = "周辺のスポット",
+    unexpected_value = "列目のデータが不正です"
+}
+
 local function split(str, div)
     if div == nil then return {str} end
 
@@ -12,29 +22,34 @@ local function split(str, div)
         result[i] = unit
         i = i + 1
     end
-  
+
     return result
-  end
+end
 
 function p.main(frame)
     local args = getArgs(frame)
 
     local wikitext = mw.html.create()
-        :wikitext(frame:extensionTag{name = 'templatestyles', args = {src = '駅一覧/styles.css'}}):done()
+        :wikitext( frame:extensionTag{ name = 'templatestyles', args = {src = i18n.css} } ):done()
         :tag( "table" ):addClass( "wikitable voy-stalist" )
             :tag( "tr" )
-                :tag( "th" ):attr( "colspan", 4 ):addClass( "wikitable voy-stalist-title" ):css( "border-bottom-color", args.color ):wikitext( args.title or title ):done()
+                :tag( "th" )
+                    :attr( "colspan", 4 )
+                    :addClass( "wikitable voy-stalist-title" )
+                    :css( "border-bottom-color", args.color )
+                    :wikitext( args.title or title )
+                    :done()
                 :done()
             :tag( "tr" )
-                :tag( "th" ):wikitext( "駅名" ):addClass( "wikitable voy-stalist-header" ):done()
-                :tag( "th" ):wikitext( "駅名" ):addClass( "wikitable voy-stalist-header" ):done()
-                :tag( "th" ):wikitext( "乗り換え路線" ):addClass( "wikitable voy-stalist-header" ):done()
-                :tag( "th" ):wikitext( "周辺のスポット" ):addClass( "wikitable voy-stalist-header" ):done()
+                :tag( "th" ):wikitext( i18n.header_num ):addClass( "wikitable voy-stalist-header" ):done()
+                :tag( "th" ):wikitext( i18n.header_name ):addClass( "wikitable voy-stalist-header" ):done()
+                :tag( "th" ):wikitext( i18n.header_tfr ):addClass( "wikitable voy-stalist-header" ):done()
+                :tag( "th" ):wikitext( i18n.header_spot ):addClass( "wikitable voy-stalist-header" ):done()
                 :done()
             for i = 1, args[i] ~= nil do
                 local datas = split(args[i], ",")
                 if datas[4] == nil then
-                    error( i .. "列目のデータが不正です" )
+                    error( i .. i18n.unexpected_value )
                 end
                 wikitext = wikitext:tag( "tr" )
                     :tag( "td" ):wikitext( datas[1] ):done()
