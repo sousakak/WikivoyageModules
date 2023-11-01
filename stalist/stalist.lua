f1f4d22fb12a71c85e96e6cb86700438e9d4bdfb
sousakak7 <sousakak7@gmail.com>
2023-11-01 20:33:05 +0900

    error handling

--- stalist/stalist.lua
+++ stalist/stalist.lua
@@ -24,7 +24,8 @@ local i18n = {
     header_spot = "周辺のスポット",
     property_num = "P154",
     property_tfr = {"P81", "P1192"},
-    unexpected_value = "列目のデータが不正です"
+    err_noitem = "ウィキデータIDが指定されていません",
+    err_wrongid = "$1番目のウィキデータIDが不正です"
 }
 
 function p.main(frame)
@@ -52,40 +53,42 @@ function p.stalist(frame)
                 :tag( "th" ):wikitext( i18n.header_tfr ):addClass( "wikitable voy-stalist-header" ):done()
                 :tag( "th" ):wikitext( i18n.header_spot ):addClass( "wikitable voy-stalist-header" ):done()
                 :done()
+    assert( args[i], i18n.err_noitem )
     i = 1
-    if args[i] ~= nil then
-        while args[i] ~= nil do
-            local qid = args[i]
-            local item = mw.wikibase.getEntity(qid) -- this is expensive and possibly stop by $wgExpensiveParserFunctionLimit
-            local value_num
-            local value_tfr = ""
+    while args[i] ~= nil do
 
-            if item["claims"][i18n.property_num] ~= nil then
-                value_num = fileLink{
-                    file    = item["claims"][i18n.property_num][1]["mainsnak"]["datavalue"]["value"],
-                    size    = "50px",
-                    link    = "",
-                    caption = item:getLabel('ja')
-                }
-            else
-                value_num = ""
-            end
-            for p = 1, #i18n.property_tfr do
-            	if item["claims"][i18n.property_tfr[p]] ~= nil then
-	                for value = 1, #item["claims"][i18n.property_tfr[p]] do
-	                    value_tfr = value_tfr .. item["claims"][i18n.property_tfr[p]][value]["mainsnak"]["datavalue"]["value"]["id"] .. "、"
-    	            end
-    	        end
-            end
-            value_tfr = value_tfr[#value_tfr - 1] -- remove the last punctuation mark
-            wikitext = wikitext:tag( "tr" ):addClass( "voy-stalist-unit voy-stalist-row" )
-                :tag( "td" ):wikitext( value_num ):done()
-                :tag( "td" ):wikitext( item:getLabel('ja') ):done()
-                :tag( "td" ):wikitext( value_tfr ):done()
-                :tag( "td" ):wikitext( args["spot" .. i] ):done()
-                :done()
-            i = i + 1
+        /* define vars */
+        local qid = string.match(args[i], "^[Qq]%d+$") or error(string.gsub(i18n.err_wrongid, "$1", i)) -- check the arg
+        local item = mw.wikibase.getEntity(qid) -- this is expensive and possibly stop by $wgExpensiveParserFunctionLimit
+        local value_num
+        local value_tfr = ""
+
+        /* get data from Wikidata */
+        if item["claims"][i18n.property_num] ~= nil then
+            value_num = fileLink {
+                file    = item["claims"][i18n.property_num][1]["mainsnak"]["datavalue"]["value"],
+                size    = "50px",
+                link    = "",
+                caption = item:getLabel('ja')
+            }
+        else
+            value_num = ""
         end
+        for p = 1, #i18n.property_tfr do
+            if item["claims"][i18n.property_tfr[p]] ~= nil then
+                for value = 1, #item["claims"][i18n.property_tfr[p]] do
+                    value_tfr = value_tfr .. item["claims"][i18n.property_tfr[p]][value]["mainsnak"]["datavalue"]["value"]["id"] .. "、"
+                end
+            end
+        end
+        value_tfr = value_tfr[#value_tfr - 1] -- remove the last punctuation mark
+        wikitext = wikitext:tag( "tr" ):addClass( "voy-stalist-unit voy-stalist-row" )
+            :tag( "td" ):wikitext( value_num ):done()
+            :tag( "td" ):wikitext( item:getLabel('ja') ):done()
+            :tag( "td" ):wikitext( value_tfr ):done()
+            :tag( "td" ):wikitext( args["spot" .. i] ):done()
+            :done()
+        i = i + 1
     end
     wikitext = wikitext:done()
     return wikitext
